e98278eb0448b8f0151734f6fdf6b81d1bb283f6
zoton2 <zoton2@gmail.com>
2019-04-08 19:51:23 +0100

    Remove DEBUG panel

--- dashboard/js/debug.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-$(() => {
-	$('.debugSDKeyUp').on('click', (evt) => {
-		evt.preventDefault();
-		var location = $(evt.currentTarget).data('location');
-		nodecg.sendMessage('debugSDKeyUp', location);
-	})
-});
